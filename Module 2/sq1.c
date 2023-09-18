c23a71302d77f69094b8c21020e84cd1e01b9a53
Hetuk <hetuk47@users.noreply.github.com>
2023-09-18 19:05:46 +0530

    squar and qube
    this commit is of file to fine squar and qube of the number

---
+++ Module 2/sq1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int squar,qube;
+
+    printf("\nEnter the number for squar:: \n");
+    scanf("%d",&squar);
+    printf("\nEnter th number for qube:: \n");
+    scanf("%d",&qube);
+
+    printf("\nThe square of a number %d is  %d\n",squar,squar*squar);
+    printf("\nThe qube of the number %d is %d\n",qube,qube*qube*qube);
+    return(0);
+}
