b2d92293d49e4b69d76501902352f996f8ffc959
Hetuk <hetuk47@users.noreply.github.com>
2023-09-27 16:47:36 +0530

    infinite calculation
    this file is with continue calculating interest rate until user wants

--- Module 2/interest.c
+++ Module 2/interest.c
@@ -1,4 +1,8 @@
+//Here clrscr() is not working with the file conio.h as this file only work
+//in dos base so we have used system("cls") with stdlib.h file.
+
 #include <stdio.h>
+#include <stdlib.h>
 
 float simple_interest(float,float,int);
 float compaund_interest(float,float,int);
@@ -7,42 +11,50 @@ int main()
 {
     float interest_rate, capital_amount, final_amount,final_interest;
     int number_of_year,break_int = 1;
-    char choose;
-
-    printf("\nPlease Enter the capital amount::");
-    scanf("%f",&capital_amount);
-    printf("\nPlease Enter the interest rate on capital amount::");
-    scanf("%f",&interest_rate);
-    printf("\nPlease enter the number of years for interest on capital amount::");
-    scanf("%d",&number_of_year);
-    printf("\nPlease enter s for simple interest and c for compaund interest::");
-    for(;;)
+    char choose, continue_calculat;
+    system("cls");
+    do
     {
-        fflush(stdin);
-        scanf("%c",&choose);
-        fflush(stdin);
-
-        switch(choose)
+        system("cls");
+        printf("\nPlease Enter the capital amount::");
+        scanf("%f",&capital_amount);
+        printf("\nPlease Enter the interest rate on capital amount::");
+        scanf("%f",&interest_rate);
+        printf("\nPlease enter the number of years for interest on capital amount::");
+        scanf("%d",&number_of_year);
+        printf("\nPlease enter s for simple interest and c for compaund interest::");
+        for(;;)
         {
-            case 's':
-                final_interest = simple_interest(capital_amount,interest_rate,number_of_year);
-                final_amount = final_interest + capital_amount;
-                break_int = 1;
-                break;
-            case 'c':
-                final_amount = compaund_interest(capital_amount,interest_rate,number_of_year);
-                final_interest = final_amount - capital_amount;
-                break_int = 1;
-                break;
-            default:
-                printf("\nPlase choos properly");
-                break_int = 0;
+            fflush(stdin);
+            scanf("%c",&choose);
+            fflush(stdin);
+
+            switch(choose)
+            {
+                case 's':
+                    final_interest = simple_interest(capital_amount,interest_rate,number_of_year);
+                    final_amount = final_interest + capital_amount;
+                    break_int = 1;
+                    break;
+                case 'c':
+                    final_amount = compaund_interest(capital_amount,interest_rate,number_of_year);
+                    final_interest = final_amount - capital_amount;
+                    break_int = 1;
+                    break;
+                default:
+                    printf("\nPlase choos properly");
+                    break_int = 0;
+                    break;
+            }
+            if(break_int == 1)
                 break;
         }
-        if(break_int == 1)
-            break;
+        printf("\nThe final amount after %d years of %f is %f with interest of %f::\n",number_of_year,capital_amount,final_amount,final_interest);
+        printf("Do you want to continue calculate other values.\nPleas press y to continue::");
+        fflush(stdin);
+        scanf("%c",&continue_calculat);
     }
-    printf("\nThe final amount after %d years of %f is %f with interest of %f::\n",number_of_year,capital_amount,final_amount,final_interest);
+    while(continue_calculat == 'y');
 
     return(0);
 }
@@ -70,17 +82,11 @@ n = number of years
 */
 float compaund_interest(float c_amount, float int_rate, int y)
 {
-    float amount = c_amount;
-    for(int i = 1; i <= y; i++)
+    float amount = 0;
+    amount = c_amount + ((c_amount) * (int_rate/100));
+    for(int i = 1; i < y; i++)
     {
-        if(i == 1)
-        {
-            amount = c_amount + ((amount) * (int_rate/100));
-        }
-        else
-        {
             amount = amount + ((amount) * (int_rate/100));
-        }
     }
     return(amount);
 }
